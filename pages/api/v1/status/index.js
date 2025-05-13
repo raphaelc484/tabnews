3dbeafa2ea9901489c8e96d3538f7c7c52af4b4f
raphaelc484 <raphaelc484@hotmail.com>
2025-05-13 17:05:54 -0300

    finaliza  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
-async function status(reqeust, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const pgVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: pgVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: usedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
