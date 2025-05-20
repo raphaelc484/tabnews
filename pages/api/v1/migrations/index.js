5da9e9d4483bc690a7c595f789912801bf80d2e9
raphaelc484 <raphaelc484@hotmail.com>
2025-05-20 11:53:38 -0300

    adds /api/v1/migrations endpoint

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,39 @@
+import migrationsRunner from "node-pg-migrate";
+import { join } from "node:path";
+import databse from "infra/database";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationsRunner(defaultMigrationsOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationsRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405);
+}
