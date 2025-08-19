b52b69fc434ebcd4aa0428f509f61758455daedb
raphaelc484 <raphaelc484@hotmail.com>
2025-08-19 13:57:02 -0300

    feat: create `user.findOneByUsername` method and `/api/v1/users/[username]` endpoint

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -11,12 +11,7 @@ export default router.handler(controller.errorHandlers);
 async function postHandler(request, response) {
   const userInputValues = request.body;
 
-  //procurar email no banco
-
-  //a busca precisa levar em consideração letra maiuscula e minuscula
-
-  //só deixar passar caso o n exista o email
-
   const newUser = await user.create(userInputValues);
+
   return response.status(201).json(newUser);
 }
