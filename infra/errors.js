6cf1ae055741cea9ac78a7d112a91c0cab179494
raphaelc484 <raphaelc484@hotmail.com>
2026-03-05 18:50:48 -0300

    refactor: improve error loggin in `email.send()`

--- infra/errors.js
+++ infra/errors.js
@@ -19,14 +19,15 @@ export class InternalServerError extends Error {
 }
 
 export class ServiceError extends Error {
-  constructor({ cause, message }) {
+  constructor({ cause, message, action, context }) {
     super(message || "Serviço indisponível no momento.", {
       cause,
     });
 
     this.name = "ServiceError";
-    this.action = "Verifique se o serviço está disponível.";
+    this.action = action || "Verifique se o serviço está disponível.";
     this.statusCode = 503;
+    this.context = context;
   }
   toJSON() {
     return {
@@ -34,6 +35,7 @@ export class ServiceError extends Error {
       message: this.message,
       action: this.action,
       status_code: this.statusCode,
+      context: this.context,
     };
   }
 }
